a0440c028534eeb9016b67eaef5add28a96cd8db
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-03-13 16:48:33 +0300

    feat: Added Date picker for the task creation

--- lib/Screens/tasks.dart
+++ lib/Screens/tasks.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 import 'package:trial_todo_app/Utils/data_function.dart';
 import 'package:trial_todo_app/Utils/textStyling.dart';
+import 'package:trial_todo_app/components/custom_textfield.dart';
 
 class TasksScreen extends StatefulWidget {
   const TasksScreen({super.key});
@@ -10,17 +13,113 @@ class TasksScreen extends StatefulWidget {
 }
 
 class _TasksScreenState extends State<TasksScreen> {
+  TextEditingController taskNameController = TextEditingController();
+  TextEditingController taskDescriptionController = TextEditingController();
+  TextEditingController startDateController = TextEditingController();
+  TextEditingController endDateController = TextEditingController();
+
+  pickDateDialog(TextEditingController? controller) async {
+    DateTime? pickedDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2101));
+
+    if (pickedDate != null) {
+      print(pickedDate);
+      String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+      print(formattedDate);
+
+      setState(() {
+        controller!.text = formattedDate;
+      });
+    } else {
+      print("Date is not selected");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create Task', style: ThemeStyling.welcomeTitle),
-        centerTitle: true,
-      ),
-      drawer: DataFunction.customMenuDrawer(context),
-      body: Center(
-        child: Text('Tasks'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Create Task', style: ThemeStyling.welcomeTitle),
+          centerTitle: true,
+        ),
+        drawer: DataFunction.customMenuDrawer(context),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20, top: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                ' Task Name',
+                style: ThemeStyling.welcomeTitle,
+              ),
+              CustomTextField(controller: taskNameController, obscure: false),
+              const SizedBox(height: 10,),
+              const Text(
+                ' Task Description',
+                style: ThemeStyling.welcomeTitle,
+              ),
+              CustomTextField(
+                controller: taskDescriptionController,
+                obscure: false,
+                maxLines: 5,
+              ),
+              const SizedBox(height: 20,),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                            controller: startDateController,
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide:
+                                      const BorderSide(color: Colors.black),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  gapPadding: 6,
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Theme.of(context).primaryColor,
+                                      width: 2),
+                                ),
+                                suffixIcon: Icon(Icons.calendar_today),
+                                labelText: "Start Date"),
+                            readOnly: true,
+                            onTap: () => pickDateDialog(startDateController)),
+                      ),
+                      const SizedBox(width: 10,),
+                      Expanded(
+                        child: TextField(
+                            controller: endDateController,
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide:
+                                      const BorderSide(color: Colors.black),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  gapPadding: 6,
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Theme.of(context).primaryColor,
+                                      width: 2),
+                                ),
+                                suffixIcon: Icon(Icons.calendar_today),
+                                labelText: "End Date"),
+                            readOnly: true,
+                            onTap: () => pickDateDialog(endDateController)),
+                      )
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/components/custom_textfield.dart
+++ lib/components/custom_textfield.dart
@@ -1,10 +1,11 @@
-
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class CustomTextField extends StatefulWidget {
   TextEditingController? controller;
-  bool  obscure;
-  CustomTextField({required this.controller, required this.obscure});
+  int? maxLines;
+  bool obscure;
+  CustomTextField({required this.controller, required this.obscure, this.maxLines});
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
@@ -14,8 +15,8 @@ class _CustomTextFieldState extends State<CustomTextField> {
   FocusNode myfocus = FocusNode();
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 0),(){
-      myfocus.requestFocus(); //auto focus on second text field.
+    Future.delayed(const Duration(seconds: 0), () {
+      myfocus.requestFocus(); 
     });
     super.initState();
   }
@@ -23,24 +24,25 @@ class _CustomTextFieldState extends State<CustomTextField> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 30.0),
+      padding: const EdgeInsets.all(5),
       child: TextField(
         // inputFormatters:[FilteringTextInputFormatter.allow(RegExp('[a-zA-Z0-9.@]'))],
         obscureText: widget.obscure,
+        maxLines: widget.maxLines ?? 1,
         controller: widget.controller,
         cursorColor: Colors.black,
-        style: const TextStyle(
-          height: 1.6,
-        ),
+        textAlignVertical: TextAlignVertical.top,
+        style: const TextStyle(height:1),
         focusNode: myfocus,
-        decoration: const InputDecoration(
-          contentPadding: EdgeInsets.only(bottom: -10.0),
-          border: InputBorder.none,
-          enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.black),
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: const BorderSide(color: Colors.black),
           ),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.black),
+          focusedBorder: OutlineInputBorder(
+            gapPadding: 6,
+            borderRadius: BorderRadius.circular(15),
+            borderSide:  BorderSide(color: Theme.of(context).primaryColor, width: 2),
           ),
         ),
       ),
