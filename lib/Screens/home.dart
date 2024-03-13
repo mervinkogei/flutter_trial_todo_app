beac58fb894424593163cab384202d62756443c8
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-03-13 15:15:39 +0300

    refactor: Refactored the home screen

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:trial_todo_app/Utils/data_function.dart';
 import 'package:trial_todo_app/widgets/searchBar_widget.dart';
+import 'package:trial_todo_app/widgets/taskCard_widget.dart';
 
 import '../Utils/textStyling.dart';
 
@@ -13,10 +16,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+ var itemCounter = 2;
+
+@override
+  void initState() {
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(statusBarColor: Colors.transparent
+        const SystemUiOverlayStyle(statusBarColor: Colors.white
             //color set to transperent or set your own color
             ));
 
@@ -54,85 +65,143 @@ class _HomeScreenState extends State<HomeScreen> {
                 padding: EdgeInsets.only(top: 5.0),
                 child: Text(' What are you doing Today!'),
               ),
-
+        
               /**
              * Added a a custom Widget of SearchBar
              */
               SearchBarWidget(),
-              const Row(
+               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
+                  const Text(
                     ' Today\'s Task!',
                     style: ThemeStyling.welcomeTitle,
                   ),
-                  Text(' see all >'),
+                  InkWell(
+                    onTap: (){
+                      setState(() {
+                        if(itemCounter ==2){
+                          setState(() {
+                            itemCounter = 12;
+                          });
+                        }else{
+                          setState(() {
+                            itemCounter =2;
+                          });
+                        }
+                      });
+
+                    },
+                    child: Text( itemCounter == 2 ? ' see all >': 'Hide ^')),
                 ],
               ),
               const SizedBox(height: 10,),
-              Column(
-                children: [
-                  GridView.count(
-                      shrinkWrap: true,
-                      crossAxisCount: 2,
-                      children: List.generate(2, (index) {
-                        return Card(
-                          color: Colors.blue[50],
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                               Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Icon(
-                                    Icons.bookmark_add_outlined,
-                                    size: 40,
-                                  ),
-                                  InkWell(
-                                    onTap: ()=> DataFunction.showAlertDialog(context),
-                                    child: Icon(
-                                      Icons.delete_outline_rounded,
-                                      color: Colors.red,
+              Container(
+                height: itemCounter != 12? 180: 350,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: GridView.count(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            crossAxisCount: 2,
+                            children: List.generate(itemCounter, (index) {
+                              return Card(
+                                color: Colors.blue[50],
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                     Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        const Icon(
+                                          Icons.bookmark_add_outlined,
+                                          size: 40,
+                                        ),
+                                        InkWell(
+                                          onTap: ()=> DataFunction.showAlertDialog(context),
+                                          child: const Icon(
+                                            Icons.delete_outline_rounded,
+                                            color: Colors.red,
+                                            size: 25,
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                  ),
-                                ],
-                              ),
-                              const Text(
-                                'Reading Article',
-                                style: ThemeStyling.taskCardTitle,
-                              ),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.center,
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text('Deadline: '),
-                                  Text('4pm'),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(15))),
-                                    onPressed: () {},
-                                    child: Text(
-                                      'Complete a Task',
-                                      textAlign: TextAlign.center,
-                                    )),
-                              )
-                            ],
-                          ),
-                        );
-                      })),
-                ],
+                                    const Text(
+                                      'Reading Article',
+                                      style: ThemeStyling.taskCardTitle,
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    const Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                          crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Text('Deadline: '),
+                                        Text('4pm'),
+                                      ],
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                              shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(15))),
+                                          onPressed: () {},
+                                          child: const Text(
+                                            'Complete a Task',
+                                            textAlign: TextAlign.center,
+                                          )),
+                                    )
+                                  ],
+                                ),
+                              );
+                            })),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Visibility(
+                visible: itemCounter == 12? false: true,
+                child: Column(
+                  children: [
+                            Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      ' YesterDay\'s Task!',
+                      style: ThemeStyling.welcomeTitle,
+                    ),
+                    InkWell(
+                      onTap: (){
+                        setState(() {
+                          if(itemCounter ==2){
+                            setState(() {
+                              itemCounter = 12;
+                            });
+                          }else{
+                            setState(() {
+                              itemCounter =2;
+                            });
+                          }
+                        });
+                
+                      },
+                      child: Text( itemCounter == 2 ? ' see all >': 'Hide ^')),
+                  ],
+                ),
+                TaskCardWidget()
+                
+                  ],
+                ),
               )
             ],
           ),
