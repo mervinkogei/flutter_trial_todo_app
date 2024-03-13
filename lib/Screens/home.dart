fedd4da7a74f9f2a29a450dd01adfa503a9a41ba
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-03-13 12:18:40 +0300

    feat: Added a custom styling components under the Utils file

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../Utils/textStyling.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -12,13 +14,12 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
-   SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      //color set to transperent or set your own color
-      statusBarIconBrightness: Brightness.dark, 
-      //set brightness for icons, like dark background light icons
-   )
-);
+       const SystemUiOverlayStyle(
+         statusBarColor: Colors.transparent
+         //color set to transperent or set your own color
+      )
+  );
+  
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -31,11 +32,11 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       drawer: Drawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
+      body: const Padding(
+        padding: EdgeInsets.only(left:20.0, right: 20),
         child: Column(
           children: [
-            Text(' Welcome To\n Task Manager!',)
+            Text(' Welcome To\n Task Manager!',style: ThemeStyling.welcomeTitle,)
           ],
         ),
       )
---
+++ lib/Utils/textStyling.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class ThemeStyling {
+  static const TextStyle welcomeTitle = TextStyle(
+      fontWeight: FontWeight.bold, fontSize: 18, fontFamily: "Roboto");
+}
