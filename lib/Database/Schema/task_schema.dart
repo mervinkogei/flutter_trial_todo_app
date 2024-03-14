d08f7bd8b56117aab618d1b81c71d37d00cc9d88
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-03-14 12:01:17 +0300

    feat: Added a schema for task and user

---
+++ lib/Database/Schema/task_schema.dart
@@ -0,0 +1,30 @@
+import 'package:quickeydb/quickeydb.dart';
+import 'package:trial_todo_app/Database/Models/task_model.dart';
+import 'package:trial_todo_app/Database/Schema/user_schema.dart';
+
+class TaskSchema extends DataAccessObject<Task> {
+   TaskSchema()
+           : super(
+      '''
+          CREATE TABLE tasks (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            taskName TEXT NOT NULL,
+            description TEXT,
+            startDate TEXT,
+            endDate TEXT,
+            status TEXT,
+            level INTEGER DEFAULT "1" NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES user (id)
+          )
+          ''',
+      relations: [
+         const BelongsTo<UserSchema>(),
+      ],
+      converter: Converter(
+         encode: (task) => Task.fromMap(task),
+         decode: (task) => task!.toMap(),
+         decodeTable: (task) => task!.toTableMap(),
+      ),
+   );
+}
---
+++ lib/Database/Schema/user_schema.dart
@@ -0,0 +1,37 @@
+import 'package:quickeydb/quickeydb.dart';
+import 'package:trial_todo_app/Database/Models/user_model.dart';
+import 'package:trial_todo_app/Database/Schema/task_schema.dart';
+
+class UserSchema extends DataAccessObject<User> {
+   UserSchema()
+           : super(
+      '''
+          CREATE TABLE user (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            password TEXT,
+          )
+          ''',
+      relations: [
+         const HasOne<TaskSchema>(),
+      ],
+      converter: Converter(
+         encode: (user) => User.fromMap(user),
+         decode: (user) => user!.toMap(),
+         decodeTable: (user) => user!.toTableMap(),
+      ),
+   );
+
+   Future<List<User?>> getOldUsers() {
+      return where({'age >= ?': 18}).toList();
+   }
+
+   Future<List<User>> doRawQuery() async {
+      // Use your custom query
+      final results = await database!.rawQuery('SELECT * FROM user');
+
+      // when returning result use converter
+      return results.map((result) => converter.encode(result) as User).toList();
+   }
+}
