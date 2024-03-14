745d8b5af1a79035613eb4c3fbd25a5d2b200700
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-03-14 10:40:45 +0300

    feat: Added a sign up screen

--- lib/Screens/signup.dart
+++ lib/Screens/signup.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:trial_todo_app/Screens/signup.dart';
+import 'package:trial_todo_app/Screens/login.dart';
 import 'package:trial_todo_app/Utils/app_Colors.dart';
 import 'package:trial_todo_app/Utils/textStyling.dart';
 import 'package:trial_todo_app/components/custom_checkBox.dart';
 import 'package:trial_todo_app/components/custom_elavatedButton.dart';
 import 'package:trial_todo_app/components/custom_textfield.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-TextEditingController emailController = TextEditingController();
-TextEditingController passwordController = TextEditingController();
+class _SignUpScreenState extends State<SignUpScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
 
-loginUser(){
-  
-}
 
   @override
   Widget build(BuildContext context) {
@@ -39,26 +38,41 @@ loginUser(){
                   
                   const SizedBox(height: 30),
                   const Text(
-                    'Welcome back 👋', style: ThemeStyling.welcomeTitle,
+                    'Hi There, Lets Onboard you 👋', style: ThemeStyling.welcomeTitle,
                   ),
                   const Text(
-                    'Please enter your email & password to sign in.',
+                    'Please enter your name,email, password & Confirm password to onboard you.',
+                    
                   ),
                   const SizedBox(
                     height: 30,
+                  ),
+                   const Text(
+                    'Name',
+                  ),
+                  CustomTextField(controller: nameController, obscure: false),
+                  const SizedBox(
+                    height: 10,
                   ),
                   const Text(
                     'Email',
                   ),
                   CustomTextField(controller: emailController, obscure: false),
                   const SizedBox(
-                    height: 20,
+                    height: 10,
                   ),
                   const Text(
                     'Password',
                   ),
             CustomTextField(controller: passwordController, obscure: true, visibilityIcon: true,),
-            const SizedBox(height: 20),
+             const SizedBox(
+                    height: 10,
+                  ),
+            const Text(
+                    'ConfirmPassword',
+                  ),
+            CustomTextField(controller: confirmPasswordController, obscure: true, visibilityIcon: true,),
+            const SizedBox(height: 10),
                   Row(
                     children: [
                       CustomCheckBox(
@@ -68,51 +82,45 @@ loginUser(){
                       const Text(
                         'Remember me',
                       ),
-                     const Spacer(),
-                      InkWell(
-                      onTap: () {},
-                      child: const Text(
-                        'Forgot password?', style: ThemeStyling.forgotPasswordText,
-                      ),
-                                          ),
+                    
                     ],
                   ),
-                  const SizedBox(
-                    height: 30,
-                  ),
+                  // const SizedBox(
+                  //   height: 30,
+                  // ),
                   const Divider(color: AppColors.kGreyScale200),
                   const SizedBox(
-                    height: 30,
+                    height: 20,
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       const Text(
-                        "Don’t have an account?",
+                        "Already have an account?",
                       ),
                       const SizedBox(width: 8),
                       InkWell(
                         onTap: () {
                           Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
-                        MaterialPageRoute(builder: (context) => const SignUpScreen()),
-                        (route) => false);
+                          MaterialPageRoute(builder: (context) => const LoginScreen()),
+                          (route) => false);
                         },
                         child: const Text(
-                          'Sign up', style: ThemeStyling.signUpText,
+                          'Sign In', style: ThemeStyling.signUpText,
                         ),
                       ),
                     ],
                   ),
                   const SizedBox(
-                    height: 30,
+                    height: 20,
                   ),
                  
                   const SizedBox(
                     height: 20,
                   ),
                   CustomElevatedButtton(
-                    callback: ()=> loginUser(),
-                    labelTitle: 'Sign in',
+                    callback: () async {},
+                    labelTitle: 'Sign Up',
                   ),
                 ],
               ),
