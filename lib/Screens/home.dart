7a73d4ce2522aa6a8bc39af453015687b11e8463
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-03-18 11:20:40 +0300

    fix: Fixed the issue on the bottom overflow

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:quickeydb/quickeydb.dart';
 import 'package:trial_todo_app/Database/Models/task_model.dart';
 import 'package:trial_todo_app/Database/Schema/task_schema.dart';
@@ -107,158 +109,112 @@ showAlertDialog(BuildContext context, taskItem) {
         
          body: Padding(
           padding: const EdgeInsets.only(left: 20.0, right: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                ' Welcome To\n Task Manager!',
-                style: ThemeStyling.welcomeTitle,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 5.0),
-                child: Text(' What are you doing Today!'),
-              ),
-        
-              /**
-             * Added a a custom Widget of SearchBar
-             */
-              SearchBarWidget(),
-               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    ' Task List!',
-                    style: ThemeStyling.welcomeTitle,
-                  ),
-                  ],
-              ),
-              const SizedBox(height: 10,),         
-              FutureBuilder<List>(
-                        future: QuickeyDB.getInstance!<TaskSchema>()?.all,
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData) {
-              return Column(
-                children: [
-                  SingleChildScrollView(
-                    child: ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, i) {
-                    // Task item = snapshot.data![i];                  
-                    return GridView.count(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        crossAxisCount: 2,
-                        children: List.generate(snapshot.data!.length, (index) {
-                          return Card(
-                            color: Colors.blue[50],
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                 
-                                 Text(
-                                  'Title: ${snapshot.data![index].taskName}',
-                                  style: ThemeStyling.taskCardTitle,
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  ' Welcome To\n Task Manager!',
+                  style: ThemeStyling.welcomeTitle,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 5.0),
+                  child: Text(' What are you doing Today!'),
+                ),
+                    
+                /**
+               * Added a a custom Widget of SearchBar
+               */
+                SearchBarWidget(),
+                 const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      ' Task List!',
+                      style: ThemeStyling.welcomeTitle,
+                    ),
+                    ],
+                ),
+                const SizedBox(height: 10,),         
+               Container(
+                 height: MediaQuery.of(context).size.height,
+                 child: FutureBuilder<List>(
+                           future: QuickeyDB.getInstance!<TaskSchema>()?.all,
+                           builder: (context, snapshot) {
+                             if (snapshot.hasData) {
+                 return Column(
+                   children: [
+                     Container(
+                       height: MediaQuery.of(context).size.height ,
+                       child: GridView.count(
+                           shrinkWrap: true,
+                           physics: const NeverScrollableScrollPhysics(),
+                           crossAxisCount: 2,
+                           children: List.generate(snapshot.data!.length, (index) {
+                             return Card(
+                               color: Colors.blue[50],
+                               child: Column(
+                                 crossAxisAlignment: CrossAxisAlignment.start,
+                                 mainAxisAlignment: MainAxisAlignment.center,
+                                 children: [
+                                    
+                                    Padding(
+                                      padding: const EdgeInsets.only(left:8.0),
+                                      child: Text(
+                                       'Title: ${snapshot.data![index].taskName}',
+                                       style: ThemeStyling.taskCardTitle,
+                                                                      ),
+                                    ),
+                                   const SizedBox(
+                                     height: 10,
+                                   ),
+                                   
+                                    Row(
+                                     mainAxisAlignment:
+                                         MainAxisAlignment.center,
+                                         crossAxisAlignment: CrossAxisAlignment.center,
+                                     children: [
+                                       Text('Deadline: '),
+                                       Text('${snapshot.data![index].endDate}'),
+                                     ],
+                                   ),
+                                   const SizedBox(
+                                     height: 10,
+                                   ),
+                                   Divider(),
+                                   Row(
+                                     mainAxisAlignment:
+                                         MainAxisAlignment.spaceEvenly,
+                                     children: [
+                                       const Icon(
+                                         Icons.edit_document,
+                                         size: 25,
+                                       ),
+                                       InkWell(
+                                         onTap: ()=> showAlertDialog(context, snapshot.data![index]),
+                                         child: const Icon(
+                                           Icons.delete_outline_rounded,
+                                           color: Colors.red,
+                                           size: 25,
+                                         ),
+                                       ),
+                                     ],
+                                   ),
                                 
-                                 Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.center,
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Text('Deadline: '),
-                                    Text('${snapshot.data![index].endDate}'),
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Divider(),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    const Icon(
-                                      Icons.edit_document,
-                                      size: 25,
-                                    ),
-                                    InkWell(
-                                      onTap: ()=> showAlertDialog(context, snapshot.data![index]),
-                                      child: const Icon(
-                                        Icons.delete_outline_rounded,
-                                        color: Colors.red,
-                                        size: 25,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                // Padding(
-                                //   padding: const EdgeInsets.all(8.0),
-                                //   child: ElevatedButton(
-                                //       style: ElevatedButton.styleFrom(
-                                //           shape: RoundedRectangleBorder(
-                                //               borderRadius:
-                                //                   BorderRadius.circular(15))),
-                                //       onPressed: () {},
-                                //       child: const Text(
-                                //         'Complete a Task',
-                                //         textAlign: TextAlign.center,
-                                //       )),
-                                // )
-                              ],
-                            ),
-                          );
-                        }));}),
-                  )
-                ]);
-                          }
-                          return const Center(child: CircularProgressIndicator());
-                          }
-                          )
-
-
-
-              // Visibility(
-              //   visible: itemCounter == 12? false: true,
-              //   child: Column(
-              //     children: [
-              //               Row(
-              //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //     children: [
-              //       Text(
-              //         ' YesterDay\'s Task!',
-              //         style: ThemeStyling.welcomeTitle,
-              //       ),
-              //       InkWell(
-              //         onTap: (){
-              //           setState(() {
-              //             if(itemCounter ==2){
-              //               setState(() {
-              //                 itemCounter = 12;
-              //               });
-              //             }else{
-              //               setState(() {
-              //                 itemCounter =2;
-              //               });
-              //             }
-              //           });
-                
-              //         },
-              //         child: Text( itemCounter == 2 ? ' see all >': 'Hide ^')),
-              //     ],
-              //   ),
-              //   TaskCardWidget()
-                
-              //     ],
-              //   ),
-              // )
+                                 ],
+                               ),
+                             );
+                           })),
+                     )
+                   ]);
+                             }
+                             return const Center(child: CircularProgressIndicator());
+                             }
+                             ),
+               )
             
-            ],
+              ],
+            ),
           ),
         ));
   }
